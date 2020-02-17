e1ca6f660fa67d0fed21e9828f8d7fb0e85fe3c3
xychenxy <mavischen916@gmail.com>
2020-02-17 22:00:55 +1100

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -5,14 +5,16 @@ import ajax from "./ajax";
 import jsonp from 'jsonp'
 import {message} from "antd";
 
+const BASE = '/api'
+
 /**
  * Login
  * @param username
  * @param password
  * @returns {*|Promise|Promise<unknown>}
  */
-export const reqLogin = (username, password) => ajax('/login', {username, password}, 'POST')
-export const reqAddUser = (user) => ajax('/manage/user/add', user, 'POST')
+export const reqLogin = (username, password) => ajax(BASE + '/login', {username, password}, 'POST')
+export const reqAddUser = (user) => ajax(BASE + '/manage/user/add', user, 'POST')
 
 /**
  * Open weather
@@ -43,9 +45,9 @@ export const reqWeather = (city) =>{
  * @param parentId
  * @returns {*|Promise|Promise<unknown>}
  */
-export const reqCategories = (parentId) => ajax('/manage/category/list', {parentId})
-export const reqAddCategory = (parentId, categoryName) => ajax('/manage/category/add',{parentId, categoryName}, 'POST')
-export const reqUpdateCategory = (categoryId, categoryName) => ajax('/manage/category/update', {categoryId, categoryName}, 'POST')
+export const reqCategories = (parentId) => ajax(BASE + '/manage/category/list', {parentId})
+export const reqAddCategory = (parentId, categoryName) => ajax(BASE + '/manage/category/add',{parentId, categoryName}, 'POST')
+export const reqUpdateCategory = (categoryId, categoryName) => ajax(BASE + '/manage/category/update', {categoryId, categoryName}, 'POST')
 
 /**
  * Products management
@@ -53,32 +55,32 @@ export const reqUpdateCategory = (categoryId, categoryName) => ajax('/manage/cat
  * @param pageSize
  * @returns {*|Promise|Promise<unknown>}
  */
-export const reqProducts = (pageNum, pageSize) => ajax('/manage/product/list', {pageNum, pageSize})
-export const reqUpdateStatus = (productId, status) => ajax( '/manage/product/updateStatus', {productId, status}, 'POST')
-export const reqAddOrUpdateProduct = (product) => ajax( '/manage/product/' + ( product._id?'update':'add'), product, 'POST')
+export const reqProducts = (pageNum, pageSize) => ajax(BASE + '/manage/product/list', {pageNum, pageSize})
+export const reqUpdateStatus = (productId, status) => ajax( BASE + '/manage/product/updateStatus', {productId, status}, 'POST')
+export const reqAddOrUpdateProduct = (product) => ajax( BASE + '/manage/product/' + ( product._id?'update':'add'), product, 'POST')
 //searchType: productName/productDesc
-export const reqSearchProducts = ({pageNum, pageSize, searchName, searchType}) => ajax('/manage/product/search',
+export const reqSearchProducts = ({pageNum, pageSize, searchName, searchType}) => ajax(BASE + '/manage/product/search',
     {
         pageNum,
         pageSize,
         [searchType]: searchName,
     })
 // get the category info
-export const reqCategory = (categoryId) => ajax('/manage/category/info', {categoryId})
-export const reqDeleteImg = (name) => ajax( '/manage/img/delete', {name}, 'POST')
+export const reqCategory = (categoryId) => ajax(BASE + '/manage/category/info', {categoryId})
+export const reqDeleteImg = (name) => ajax( BASE + '/manage/img/delete', {name}, 'POST')
 
 /**
  * Roles management
  * @returns {*|Promise|Promise<unknown>}
  */
-export const reqRoles = () => ajax('/manage/role/list')
-export const reqAddRole = (roleName) => ajax( '/manage/role/add', {roleName}, 'POST')
-export const reqUpdateRole = (role) => ajax('/manage/role/update', role, 'POST')
+export const reqRoles = () => ajax(BASE + '/manage/role/list')
+export const reqAddRole = (roleName) => ajax( BASE + '/manage/role/add', {roleName}, 'POST')
+export const reqUpdateRole = (role) => ajax(BASE + '/manage/role/update', role, 'POST')
 
 /**
  * Users management
  * @returns {*|Promise|Promise<unknown>}
  */
-export const reqUsers = () => ajax('/manage/user/list')
-export const reqDeleteUser = (userId) => ajax(  '/manage/user/delete', {userId}, 'POST')
-export const reqAddOrUpdateUser = (user) => ajax('/manage/user/'+(user._id ? 'update' : 'add'), user, 'POST')
+export const reqUsers = () => ajax(BASE + '/manage/user/list')
+export const reqDeleteUser = (userId) => ajax(  BASE + '/manage/user/delete', {userId}, 'POST')
+export const reqAddOrUpdateUser = (user) => ajax(BASE + '/manage/user/'+(user._id ? 'update' : 'add'), user, 'POST')
